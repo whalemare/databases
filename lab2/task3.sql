2eff162baf576c22f7d86c4cac4cd065cb498764
whalemare <nanostet@gmail.com>
2019-04-22 18:18:46 +0700

    Add task 3

--- lab2/task3.sql
+++ lab2/task3.sql
@@ -1,1 +1,14 @@
 -- Найти всех мастеров, чья работа стоит более 500 р.
+create or replace view expensive_masters as (
+  select masters.id,
+         name,
+         lastname,
+         birthday,
+         address,
+         price
+  from masters
+         inner join items on masters.item = items.id
+  where items.price > 500
+);
+
+select * from expensive_masters;
