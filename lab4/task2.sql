b77ae3c037aab366e3740d449e1ff20cff3c1a75
whalemare <nanostet@gmail.com>
2019-05-19 16:43:13 +0700

    Add count shops repair item

--- lab4/task2.sql
+++ lab4/task2.sql
@@ -1,14 +1,30 @@
+--
 -- Обеспеченность каждого города мастерскими
+--
 
 -- Форматированный вывод списка городов
 select shops.id, shops.name, cities.name
 from shops
-         inner join cities on shops.city = cities.id
+         inner join cities on shops.city = cities.id;
 
 -- Количество мастерских в городах
+select cities.name, count(shops.city) as count
+from shops
+         inner join cities on shops.city = cities.id
+group by cities.name
+order by count;
 
+-- Мастерские ремонтирующие товар N
+create or replace view shops_repair_n as (
+    select shops.name, items.type
+    from shop_can_repair
+             inner join items on shop_can_repair.item = items.id
+             inner join shops on shop_can_repair.shop = shops.id
+    where item = 1
+);
 
 -- Количество мастерских ремонтирующих товар N
+select count(*) from shops_repair_n;
 
 -- Количество мастерских ремонтирующих товары А и Б
 
